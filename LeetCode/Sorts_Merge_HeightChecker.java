f7906c6d1fbe67144b3d0ccaecc15429fa24314e
jeff-tran34 <50462214+jeff-tran34@users.noreply.github.com>
2025-09-10 11:15:20 -0700

    Create Sorts_Merge_HeightChecker.java

---
+++ LeetCode/Sorts_Merge_HeightChecker.java
@@ -0,0 +1,24 @@
+class Solution {
+
+
+
+
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int[] expected = new int[n];
+        
+        for(int i = 0; i< n; i++){
+            expected[i] = heights[i];
+        }
+        Arrays.sort(expected);
+        //bubbleSort(expected);
+        
+        int output = 0;
+        for(int i = 0; i < n; i++){
+            if(heights[i] != expected[i]){
+                output++;
+            }
+        }
+        return output;
+    }
+}

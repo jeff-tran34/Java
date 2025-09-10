3a390e147844f59f915790feb84d074a9547d1a8
jeff-tran34 <50462214+jeff-tran34@users.noreply.github.com>
2025-09-10 11:13:35 -0700

    Update and rename Sorts_BubbleMergeHeapCountingRadix_HeightChecker.java to Sorts_Bubble_HeightChecker.java

---
+++ LeetCode/Sorts_Bubble_HeightChecker.java
@@ -0,0 +1,37 @@
+class Solution {
+    private void bubbleSort(int[] arr){
+        int n = arr.length;
+        // Outer loop, i reps how many elements are sorted from end
+        for(int i = 0; i < n - 1; ++i){
+            for(int j = 0; j < n - i - 1; ++j){
+                // Compare and swap, if necessary
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int[] expected = new int[n];
+        
+        for(int i = 0; i< n; i++){
+            expected[i] = heights[i];
+        }
+        //Arrays.sort(expected);
+        bubbleSort(expected);
+        
+        int output = 0;
+        for(int i = 0; i < n; i++){
+            if(heights[i] != expected[i]){
+                output++;
+            }
+        }
+        return output;
+    }
+}
